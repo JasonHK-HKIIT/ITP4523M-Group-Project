70ba4b96e8b0224673dd11f8112db8639725e621
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-03-29 00:56:48 +0800

    Fixed typo

--- htdocs/admin/materials/edit.php
+++ htdocs/admin/materials/edit.php
@@ -4,6 +4,6 @@ $action = $_GET["action"];
 
 $tpl = $_SERVER["DOCUMENT_ROOT"] . "/admin/materials/edit.tpl.php";
 $navbar_menu_tpl = $_SERVER["DOCUMENT_ROOT"] . "/admin/_navbar.tpl.php";
-$page_title = (($action === "edit") ? "Edit" : "New") . " Materials";
+$page_title = (($action === "edit") ? "Edit" : "New") . " Material";
 
 require_once($_SERVER["DOCUMENT_ROOT"] . "/_base.tpl.php");
