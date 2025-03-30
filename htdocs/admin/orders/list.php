60d4c4e05b91293518467de146724b0d45b6a3e9
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-03-31 02:08:08 +0800

    Added orders list

---
+++ htdocs/admin/orders/list.php
@@ -0,0 +1,21 @@
+<?php
+
+$orders = [
+    ["id" => 1, "date" => "2025-03-10", "client_name" => "John Doe", "total" => 2500, "status" => "Open", "delivery_date" => ""],
+    ["id" => 1, "date" => "2025-03-10", "client_name" => "John Doe", "total" => 2500, "status" => "Open", "delivery_date" => ""],
+    ["id" => 1, "date" => "2025-03-10", "client_name" => "John Doe", "total" => 2500, "status" => "Open", "delivery_date" => ""],
+    ["id" => 1, "date" => "2025-03-10", "client_name" => "John Doe", "total" => 2500, "status" => "Open", "delivery_date" => ""],
+    ["id" => 1, "date" => "2025-03-10", "client_name" => "John Doe", "total" => 2500, "status" => "Open", "delivery_date" => ""],
+    ["id" => 1, "date" => "2025-03-10", "client_name" => "John Doe", "total" => 2500, "status" => "Open", "delivery_date" => ""],
+    ["id" => 1, "date" => "2025-03-10", "client_name" => "John Doe", "total" => 2500, "status" => "Open", "delivery_date" => ""],
+    ["id" => 1, "date" => "2025-03-10", "client_name" => "John Doe", "total" => 2500, "status" => "Open", "delivery_date" => ""],
+    ["id" => 1, "date" => "2025-03-10", "client_name" => "John Doe", "total" => 2500, "status" => "Open", "delivery_date" => ""],
+    ["id" => 1, "date" => "2025-03-10", "client_name" => "John Doe", "total" => 2500, "status" => "Open", "delivery_date" => ""],
+    ["id" => 1, "date" => "2025-03-10", "client_name" => "John Doe", "total" => 2500, "status" => "Open", "delivery_date" => ""],
+];
+
+$tpl = $_SERVER["DOCUMENT_ROOT"] . "/admin/orders/list.tpl.php";
+$navbar_menu_tpl = $_SERVER["DOCUMENT_ROOT"] . "/admin/_navbar.tpl.php";
+$page_title = "Orders";
+
+require_once($_SERVER["DOCUMENT_ROOT"] . "/_base.tpl.php");
