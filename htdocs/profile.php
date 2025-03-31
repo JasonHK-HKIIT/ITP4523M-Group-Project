2d17ae2fa3f93f34fdc085eb7b9220fea7c19029
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-04-01 00:51:49 +0800

    Added client profile edit page

---
+++ htdocs/profile.php
@@ -0,0 +1,7 @@
+<?php
+
+$tpl = $_SERVER["DOCUMENT_ROOT"] . "/profile.tpl.php";
+$navbar_menu_tpl = $_SERVER["DOCUMENT_ROOT"] . "/_navbar.tpl.php";
+$page_title = "Profile";
+
+require_once($_SERVER["DOCUMENT_ROOT"] . "/_base.tpl.php");
