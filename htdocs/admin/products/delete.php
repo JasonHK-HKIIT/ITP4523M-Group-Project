6da3471ebd17d54048c88dd86356d6a493388737
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-04-11 22:44:15 +0800

    Updated delete product page

--- htdocs/admin/products/delete.php
+++ htdocs/admin/products/delete.php
@@ -1,13 +1,29 @@
 <?php
 
+$navbar_menu_tpl = $_SERVER["DOCUMENT_ROOT"] . "/admin/_navbar.tpl.php";
+
 require_once $_SERVER["DOCUMENT_ROOT"] . "/_database.php";
 
 $statement = $database->prepare("SELECT `pid`, `pname` FROM `product` WHERE `pid` = ?");
 $statement->execute([$_GET["id"]]);
 $result = $statement->get_result();
+if ($result->num_rows == 0)
+{
+    http_response_code(404);
+
+    $error_title = "Product Not Found";
+    $error_message = "The requested product does not exist.";
+
+    $tpl = $_SERVER["DOCUMENT_ROOT"] . "/_error.tpl.php";
+    $page_title = "Error";
+    $navbar_theme = "danger";
+    require_once($_SERVER["DOCUMENT_ROOT"] . "/_base.tpl.php");
+
+    exit();
+}
+
 $product = $result->fetch_assoc();
 
-$navbar_menu_tpl = $_SERVER["DOCUMENT_ROOT"] . "/admin/_navbar.tpl.php";
 $navbar_theme = "warning";
 
 $tpl = $_SERVER["DOCUMENT_ROOT"] . "/admin/products/delete.tpl.php";
--- htdocs/admin/products/delete.tpl.php
+++ htdocs/admin/products/delete.tpl.php
@@ -1,7 +1,9 @@
 <main class="hero is-warning is-fullheight-with-navbar">
     <div class="hero-body">
         <form class="container is-max-desktop has-text-centered" action="/admin/products.php?action=delete" method="post" type="application/x-www-form-urlencoded">
-            <input type="hidden" name="id" value="<?= $product["pid"] ?>" />
+            
+            <input name="pid" value="<?= $product["pid"] ?>" type="hidden" />
+
             <h1 class="title">
                 Delete Product
             </h1>
@@ -22,6 +24,7 @@
                     </button>
                 </div>
             </p>
+            
         </form>
     </div>
 </main>
