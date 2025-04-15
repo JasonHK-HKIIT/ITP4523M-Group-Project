d0e464a50c49fd429d1c70941e8446d069ba17cb
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-04-16 00:25:50 +0800

    Added error page render

--- htdocs/_global.php
+++ htdocs/_global.php
@@ -4,14 +4,26 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/_database.php";
 
 $IS_ADMIN_PAGE = str_starts_with($_SERVER["REQUEST_URI"], "/admin/");
 
-function render_page(string $template_path, string $title = "", array $vars = []): void
+function render_page(string $template_path, string $title = "", array $vars = [], string $theme = "primary"): void
 {
     global $IS_ADMIN_PAGE;
 
     $tpl = $_SERVER["DOCUMENT_ROOT"] . $template_path;
-    $navbar_menu_tpl = $_SERVER["DOCUMENT_ROOT"] . ($IS_ADMIN_PAGE ? "/admin/_navbar.tpl.php" : "/_navbar.tpl.php");
     $page_title = $title;
 
+    $navbar_menu_tpl = $_SERVER["DOCUMENT_ROOT"] . ($IS_ADMIN_PAGE ? "/admin/_navbar.tpl.php" : "/_navbar.tpl.php");
+    $navbar_theme = $theme;
+
+    unset($template_path, $title, $theme);
     extract($vars, EXTR_SKIP);
     require_once $_SERVER["DOCUMENT_ROOT"] . "/_base.tpl.php";
+}
+
+function render_error_page(string $title, string $message, string $page_title = "Error")
+{
+    render_page(
+        "/_error.tpl.php", 
+        $page_title, 
+        ["error_title" => $title, "error_message" => $message], 
+        "danger");
 }
