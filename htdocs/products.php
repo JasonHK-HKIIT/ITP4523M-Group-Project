3c199bd4ffc11f2c67ef5428c9a515ba38ddb0c7
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-04-15 23:51:36 +0800

    Updated client products page

--- htdocs/products.php
+++ htdocs/products.php
@@ -1,14 +1,10 @@
 <?php
 
-require_once $_SERVER["DOCUMENT_ROOT"] . "/_database.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/_global.php";
 
-$statement = $database->prepare("SELECT * FROM `product`");
+$statement = $database->prepare("SELECT `pid`, `pname`, `pdesc`, `pcost` FROM `product`");
 $statement->execute();
 $result = $statement->get_result();
 $products = $result->fetch_all(MYSQLI_ASSOC);
 
-$tpl = $_SERVER["DOCUMENT_ROOT"] . "/products.tpl.php";
-$navbar_menu_tpl = $_SERVER["DOCUMENT_ROOT"] . "/_navbar.tpl.php";
-$page_title = "Products";
-
-require_once($_SERVER["DOCUMENT_ROOT"] . "/_base.tpl.php");
+render_page("/products.tpl.php", "Products", ["products" => $products]);
--- htdocs/products.tpl.php
+++ htdocs/products.tpl.php
@@ -12,7 +12,7 @@
     <div class="container is-max-desktop">
         <div class="list has-visible-pointer-controls">
             <? foreach ($products as $product): ?>
-                <div class="list-item">
+                <div class="list-item box is-align-items-flex-start">
                     <div class="list-item-image">
                         <figure class="image is-96x96">
                             <img src="/assets/products/<?= $product["pid"] ?>.jpg" alt="<?= $product["pname"] ?>">
@@ -20,21 +20,22 @@
                     </div>
                     <div class="list-item-content">
                         <div class="list-item-title"><?= $product["pname"] ?></div>
+                        <div class="list-item-description"><?= $product["pdesc"] ?></div>
                     </div>
                     <div class="list-item-controls">
                         <div class="is-flex is-align-items-center">
                             <div class="field has-addons mb-0">
                                 <p class="control">
-                                    <input class="input" type="number" size="5" value="1" min="1">
-                                </p>
-                                <p class="control">
                                     <a class="button is-static" data-price="<?= doubleval($product["pcost"]) ?>">
                                         $<?= sprintf("%.2f", doubleval($product["pcost"])) ?>
                                     </a>
                                 </p>
+                                <p class="control">
+                                    <input class="input" type="number" size="2" value="1" min="1">
+                                </p>
                             </div>
                             <div class="buttons ml-3">
-                                <button class="button" title="Add to Cart" aria-label="Add to Cart">
+                                <button class="button is-primary is-outlined" title="Add to Cart" aria-label="Add to Cart">
                                     <span class="icon">
                                         <i class="fa-solid fa-cart-plus"></i>
                                     </span>
