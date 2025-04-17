09374d63ae1c6ed7751cff9657b71d70eecebabe
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-04-17 12:37:45 +0800

    Added client register function

--- htdocs/register.php
+++ htdocs/register.php
@@ -2,4 +2,74 @@
 
 require_once $_SERVER["DOCUMENT_ROOT"] . "/_global.php";
 
-render_page("/register.tpl.php", "Register");
+ensure_logged_out();
+
+$field_values = [];
+$error_messages = [];
+
+$client = [];
+
+if ($_SERVER["REQUEST_METHOD"] === "POST")
+{
+    if ((!empty($_POST["cname"]) && (strlen($_POST["cname"]) <= 255))
+        && ((!empty($_POST["cpassword"]) && (strlen($_POST["cpassword"]) <= 255)) && !empty($_POST["cpassword_confirm"]) && ($_POST["cpassword"] === $_POST["cpassword_confirm"]))
+        && (empty($_POST["ctel"]) || is_telephone($_POST["ctel"])))
+    {
+        $statement = $database->prepare("INSERT INTO `customer` (`cname`, `cpassword`, `ctel`, `caddr`, `company`) VALUES (?, ?, ?, ?, ?)");
+        $statement->execute([$_POST["cname"], $_POST["cpassword"], $_POST["ctel"] ?: null, $_POST["caddr"], $_POST["company"] ?: null]);
+        $result = $statement->store_result();
+        if ($statement->affected_rows == 0)
+        {
+            http_response_code(500);
+            render_error_page("Internal Server Error", "The request failed due to unknown reason.");
+            exit;
+        }
+
+        $_SESSION["user_type"] = USER_CLIENT;
+        $_SESSION["user_id"] = $statement->insert_id;
+
+        header(sprintf("Location: /%s", $_GET["return"] ?? ""), true, 307);
+        exit;
+    }
+    else
+    {
+        $client["cname"] = $_POST["cname"];
+        $client["ctel"] = $_POST["ctel"];
+        $client["caddr"] = $_POST["caddr"];
+        $client["company"] = $_POST["company"];
+
+        if (empty($_POST["cname"]))
+        {
+            $error_messages["cname"] = "This field is required";
+        }
+        else if (strlen($_POST["cname"]) > 255)
+        {
+            $error_messages["cname"] = "The name is too long";
+        }
+
+        if (empty($_POST["cpassword"]))
+        {
+            $error_messages["cpassword"] = "This field is required";
+        }
+        else if (strlen($_POST["cpassword"]) > 255)
+        {
+            $error_messages["cname"] = "The password is too long";
+        }
+
+        if (empty($_POST["cpassword_confirm"]))
+        {
+            $error_messages["cpassword_confirm"] = "This field is required";
+        }
+        else if ($_POST["cpassword_confirm"] !== $_POST["cpassword"])
+        {
+            $error_messages["cpassword_confirm"] = "The password does not match";
+        }
+
+        if (!empty($_POST["ctel"]) && !is_telephone(empty($_POST["ctel"])))
+        {
+            $error_messages["ctel"] = "Invalid telephone number";
+        }
+    }
+}
+
+render_page("/register.tpl.php", "Register", compact("client", "error_messages"));
--- htdocs/register.tpl.php
+++ htdocs/register.tpl.php
@@ -11,13 +11,16 @@
 
         <div class="field is-horizontal">
             <div class="field-label is-normal">
-                <label for="username" class="label">Username</label>
+                <label for="username" class="label">Name</label>
             </div>
             <div class="field-body">
                 <div class="field is-narrow">
                     <p class="control">
-                        <input id="username" class="input" name="username" type="text" autocomplete="username" placeholder="Username" required />
+                        <input id="username" class="input<?= isset($error_messages["cname"]) ? " is-danger" : "" ?>" name="cname" value="<?= @$client["cname"] ?>" type="text" autocomplete="name" placeholder="Name" required />
                     </p>
+                    <? if (isset($error_messages["cname"])): ?>
+                        <p class="help is-danger"><?= $error_messages["cname"] ?></p>
+                    <? endif ?>
                 </div>
             </div>
         </div>
@@ -29,13 +32,19 @@
             <div class="field-body">
                 <div class="field is-narrow">
                     <p class="control">
-                        <input id="password" class="input" name="password" type="password" autocomplete="new-password" placeholder="Password" required />
+                        <input id="password" class="input<?= isset($error_messages["cpassword"]) ? " is-danger" : "" ?>" name="cpassword" type="password" autocomplete="new-password" placeholder="Password" required />
                     </p>
+                    <? if (isset($error_messages["cpassword"])): ?>
+                        <p class="help is-danger"><?= $error_messages["cpassword"] ?></p>
+                    <? endif ?>
                 </div>
                 <div class="field is-narrow">
                     <p class="control">
-                        <input id="confirm-password" class="input" name="confirm_password" type="password" autocomplete="new-password" placeholder="Confirm Password" required />
+                        <input id="confirm-password" class="input<?= isset($error_messages["cpassword_confirm"]) ? " is-danger" : "" ?>" name="cpassword_confirm" type="password" autocomplete="new-password" placeholder="Confirm Password" required />
                     </p>
+                    <? if (isset($error_messages["cpassword_confirm"])): ?>
+                        <p class="help is-danger"><?= $error_messages["cpassword_confirm"] ?></p>
+                    <? endif ?>
                 </div>
             </div>
         </div>
@@ -47,21 +56,11 @@
             <div class="field-body">
                 <div class="field is-narrow">
                     <p class="control">
-                        <input id="telephone" class="input" name="telephone" type="tel" size="8" placeholder="Telephone" required />
+                        <input id="telephone" class="input<?= isset($error_messages["ctel"]) ? " is-danger" : "" ?>" name="ctel" value="<?= @$client["ctel"] ?>" type="tel" size="8" maxlength="8" placeholder="Telephone" />
                     </p>
-                </div>
-            </div>
-        </div>
-
-        <div class="field is-horizontal">
-            <div class="field-label is-normal">
-                <label for="company-name" class="label">Company Name</label>
-            </div>
-            <div class="field-body">
-                <div class="field is-narrow">
-                    <p class="control">
-                        <input id="company-name" class="input" name="company_name" type="text" placeholder="Company Name" required />
-                    </p>
+                    <? if (isset($error_messages["ctel"])): ?>
+                        <p class="help is-danger"><?= $error_messages["ctel"] ?></p>
+                    <? endif ?>
                 </div>
             </div>
         </div>
@@ -73,8 +72,27 @@
             <div class="field-body">
                 <div class="field">
                     <p class="control is-expanded">
-                        <textarea id="address" name="address" class="textarea" rows="3" required></textarea>
+                        <textarea id="address" name="caddr" class="textarea" rows="3"><?= @$client["caddr"] ?></textarea>
                     </p>
+                    <? if (isset($error_messages["caddr"])): ?>
+                        <p class="help is-danger"><?= $error_messages["caddr"] ?></p>
+                    <? endif ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="field is-horizontal">
+            <div class="field-label is-normal">
+                <label for="company-name" class="label">Company Name</label>
+            </div>
+            <div class="field-body">
+                <div class="field is-narrow">
+                    <p class="control">
+                        <input id="company-name" class="input" name="company" value="<?= @$client["company"] ?>" type="text" placeholder="Company Name" />
+                    </p>
+                    <? if (isset($error_messages["company"])): ?>
+                        <p class="help is-danger"><?= $error_messages["company"] ?></p>
+                    <? endif ?>
                 </div>
             </div>
         </div>
