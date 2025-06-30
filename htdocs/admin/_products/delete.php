a6934d15c880c24b6e4b73a816aef71422a51eee
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-06-30 23:49:22 +0800

    Added product deletion

--- htdocs/admin/_products/delete.php
+++ htdocs/admin/_products/delete.php
@@ -2,8 +2,9 @@
 
 require_once $_SERVER["DOCUMENT_ROOT"] . "/_global.php";
 
+$pid = $_POST["pid"] ?? $_GET["id"];
 $statement = $database->prepare("SELECT `pid`, `pname` FROM `product` WHERE `pid` = ?");
-$statement->bind_param("i", $_GET["id"]);
+$statement->bind_param("i", $pid);
 $statement->execute();
 
 $result = $statement->get_result();
@@ -13,6 +14,60 @@ if ($result->num_rows == 0)
     render_error_page("Product Not Found", "The requested product does not exist.");
     exit;
 }
-
 $product = $result->fetch_assoc();
+
+$statement = $database->prepare("SELECT `oid` FROM `orders` WHERE `pid` = ?");
+$statement->bind_param("i", $pid);
+$statement->execute();
+
+$result = $statement->get_result();
+if ($result->num_rows > 0)
+{
+    http_response_code(400);
+    render_error_page("Product In Use", sprintf("Delete the %d order(s) ordered this product first before continue.", $result->num_rows));
+    exit;
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST")
+{
+    if (empty($_POST["pid"]))
+    {
+        http_response_code(400);
+        render_error_page("Invalid Request", "The request received was invalid.");
+        exit;
+    }
+
+    $database->begin_transaction();
+    try
+    {
+        $statement = $database->prepare("DELETE FROM `prodmat` WHERE `pid` = ?");
+        $statement->bind_param("i", $_POST["pid"]);
+        $statement->execute();
+
+        $statement = $database->prepare("DELETE FROM `product` WHERE `pid` = ?");
+        $statement->bind_param("i", $_POST["pid"]);
+        $statement->execute();
+
+        $statement->store_result();
+        if ($statement->affected_rows == 0)
+        {
+            $database->rollback();
+            http_response_code(500);
+            render_error_page("Product Not Deleted", "The request failed due to unknown reason.");
+            exit;
+        }
+    }
+    catch (mysqli_sql_exception $ex)
+    {
+        $database->rollback();
+        http_response_code(500);
+        render_error_page(sprintf("MySQL Error %d", $ex->getCode()), $ex->getMessage());
+        exit;
+    }
+    
+    $database->commit();
+    header("Location: /admin/products.php", true, 308);
+    exit;
+}
+
 render_page("/admin/_products/delete.tpl.php", "Delete Product", compact("product"), "warning");
