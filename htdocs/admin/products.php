71df5ac649b0b61913abfd86e984528ebda48109
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-07-01 01:48:07 +0800

    Updated `require_once`s

--- htdocs/admin/products.php
+++ htdocs/admin/products.php
@@ -10,14 +10,14 @@ if (!isset($_GET["action"])) { $_GET["action"] = "list"; }
 switch ($_GET["action"])
 {
     case "list":
-        require_once($_SERVER["DOCUMENT_ROOT"] . "/admin/products/list.php");
+        require_once($_SERVER["DOCUMENT_ROOT"] . "/admin/_products/list.php");
         break;
     case "edit":
     case "new":
-        require_once($_SERVER["DOCUMENT_ROOT"] . "/admin/products/edit.php");
+        require_once($_SERVER["DOCUMENT_ROOT"] . "/admin/_products/edit.php");
         break;
     case "delete":
-        require_once($_SERVER["DOCUMENT_ROOT"] . "/admin/products/delete.php");
+        require_once($_SERVER["DOCUMENT_ROOT"] . "/admin/_products/delete.php");
         break;
     default:
         header("Location: /admin/products.php", true, 301);
