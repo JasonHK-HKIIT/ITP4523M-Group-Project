882d3cd86deecafb64ea29a0c96bcfd38c2d65c1
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-07-02 22:56:14 +0800

    Added new order page for customers

---
+++ htdocs/_orders/new.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once $_SERVER["DOCUMENT_ROOT"] . "/_global.php";
+
+$error_messages = [];
+$max_quantity = PHP_INT_MAX;
+
+$pid = $_POST["pid"] ?? $_GET["pid"];
+$statement = $database->prepare("SELECT *, MIN(`pqty`) AS `pqty` FROM (SELECT `product`.`pid`, `pname`, `pcost`, FLOOR((`mqty` - `mrqty`) / `pmqty`) AS `pqty` FROM `product` CROSS JOIN `prodmat` ON `product`.`pid` = `prodmat`.`pid` LEFT JOIN `material` ON `prodmat`.`mid` = `material`.`mid`) `p` WHERE `pid` = ? GROUP BY `pid` HAVING `pqty` > 0");
+$statement->bind_param("i", $pid);
+$statement->execute();
+
+$result = $statement->get_result();
+$product = $result->fetch_assoc();
+
+$statement = $database->prepare("SELECT `caddr` FROM `customer` WHERE `cid` = ?");
+$statement->bind_param("i", $_SESSION["user_id"]);
+$statement->execute();
+
+$result = $statement->get_result();
+$client = $result->fetch_assoc();
+
+$order = [];
+
+if ($_SERVER["REQUEST_METHOD"] === "POST")
+{
+    
+}
+else
+{
+
+}
+
+render_page("/_orders/new.tpl.php", "New Order", compact("order", "product", "client", "error_messages"));
---
+++ htdocs/_orders/new.tpl.php
@@ -0,0 +1,111 @@
+<style>
+    .textarea.is-static[readonly]
+    {
+
+    }
+</style>
+
+<header class="hero is-primary is-medium">
+    <div class="hero-body container is-max-desktop">
+        <h1 class="title">
+            New Order
+        </h1>
+    </div>
+</header>
+
+<main class="m-4 mt-5">
+    <form class="container is-max-desktop" action="<?= $_SERVER["REQUEST_URI"] ?>" method="post" enctype="application/x-www-form-urlencoded">
+
+        <div class="field is-horizontal">
+            <div class="field-label is-flex is-flex-direction-column is-justify-content-center">
+                <label class="label">Product</label>
+            </div>
+            <div class="field-body">
+                <div class="field">
+                    <div class="is-flex is-flex-direction-row is-flex-grow-1 is-align-items-center">
+                        <figure class="image is-96x96">
+                            <img src="/assets/products/<?= $product["pid"] ?>.jpg" alt="<?= htmlspecialchars($product["pname"]) ?>">
+                        </figure>
+                        <p class="ml-4"><?= htmlspecialchars($product["pname"]) ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="field is-horizontal">
+            <div class="field-label is-normal">
+                <label for="quantity" class="label">Quantity</label>
+            </div>
+            <div class="field-body">
+                <div class="field is-narrow">
+                    <p class="control">
+                        <input id="quantity" class="input" name="oqty" value="<?= $order["oqty"] ?? 1 ?>" type="number" size="4" min="1" max="<?= $product["pqty"] ?>" />
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="field is-horizontal">
+            <div class="field-label">
+                <label class="label">Total Amount</label>
+            </div>
+            <div class="field-body">
+                <div class="field">
+                    <p id="total-amount" data-pcost="<?= $product["pcost"] ?>">
+                        <?= sprintf("\$%.2f", $product["pcost"]) ?>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="field is-horizontal">
+            <div class="field-label">
+                <label class="label">Delivery Address</label>
+            </div>
+            <div class="field-body">
+                <div class="field is-expended">
+                    <p class="control">
+                        <textarea class="textarea is-static p-0" rows="3" readonly style="border: none; outline: none;"><?= htmlspecialchars($client["caddr"]) ?></textarea>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="field is-horizontal">
+            <div class="field-label"></div>
+            <div class="field-body">
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button is-primary" type="submit">
+                            <span class="icon is-small">
+                                <i class="fa-solid fa-cart-shopping"></i>
+                            </span>
+                            <span>Place Order</span>
+                        </button>
+                    </div>
+                    <div class="control">
+                        <button class="button is-light is-cancel">
+                            <span class="icon is-small">
+                                <i class="fa-solid fa-xmark"></i>
+                            </span>
+                            <span>Cancel</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </form>
+</main>
+
+<script>
+    document.getElementById("quantity").addEventListener("change", function(event)
+    {
+        const quantity = this.valueAsNumber;
+
+        const totalAmountField = document.getElementById("total-amount");
+        totalAmountField.innerText = `\$${(Number.parseFloat(totalAmountField.dataset.pcost) * quantity).toFixed(2)}`;
+    });
+</script>
+
+<script src="/assets/forms.js" defer async></script>
