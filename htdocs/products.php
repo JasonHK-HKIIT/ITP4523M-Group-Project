7e3c88909350355af4b954341cd108357bda83a0
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-07-02 22:57:17 +0800

    Updated query

--- htdocs/products.php
+++ htdocs/products.php
@@ -2,7 +2,7 @@
 
 require_once $_SERVER["DOCUMENT_ROOT"] . "/_global.php";
 
-$result = $database->query("SELECT *, MIN(`pqty`) AS `pqty` FROM (SELECT `product`.`pid`, `pname`, `pdesc`, `pcost`, FLOOR(`mqty` / `pmqty`) AS `pqty` FROM `product` CROSS JOIN `prodmat` ON `product`.`pid` = `prodmat`.`pid` LEFT JOIN `material` ON `prodmat`.`mid` = `material`.`mid`) `p` GROUP BY `pid` HAVING `pqty` > 0");
+$result = $database->query("SELECT *, MIN(`pqty`) AS `pqty` FROM (SELECT `product`.`pid`, `pname`, `pdesc`, `pcost`, FLOOR((`mqty` - `mrqty`) / `pmqty`) AS `pqty` FROM `product` CROSS JOIN `prodmat` ON `product`.`pid` = `prodmat`.`pid` LEFT JOIN `material` ON `prodmat`.`mid` = `material`.`mid`) `p` GROUP BY `pid` HAVING `pqty` > 0");
 $products = $result->fetch_all(MYSQLI_ASSOC);
 
 render_page("/products.tpl.php", "Products", ["products" => $products]);
