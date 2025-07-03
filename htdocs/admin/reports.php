03541045a93b94b1eef2c445c74714e2083be8c3
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-07-03 20:51:20 +0800

    Updated /admin/reports.php

--- htdocs/admin/reports.php
+++ htdocs/admin/reports.php
@@ -1,14 +1,15 @@
 <?php
 
-$products = [
-    ["name" => "Toy Car", "total_orders" => 100, "total_sales" => 10000],
-    ["name" => "Toy Car", "total_orders" => 100, "total_sales" => 10000],
-    ["name" => "Toy Car", "total_orders" => 100, "total_sales" => 10000],
-    ["name" => "Toy Car", "total_orders" => 100, "total_sales" => 10000],
-];
+require_once $_SERVER["DOCUMENT_ROOT"] . "/_global.php";
 
-$tpl = $_SERVER["DOCUMENT_ROOT"] . "/admin/reports.tpl.php";
-$navbar_menu_tpl = $_SERVER["DOCUMENT_ROOT"] . "/admin/_navbar.tpl.php";
-$page_title = "Reports";
+ensure_logged_in();
+ensure_staff();
 
-require_once($_SERVER["DOCUMENT_ROOT"] . "/_base.tpl.php");
+$result = $database->query("SELECT `orders`.`pid`, `pname`, COUNT(*) AS `ocount`, SUM(`ocost`) AS `ocost` FROM `orders` LEFT JOIN `product` ON `orders`.`pid` = `product`.`pid` GROUP BY `orders`.`pid`");
+$products = $result->fetch_all(MYSQLI_ASSOC);
+
+$result = $database->query("SELECT `orders`.`cid`, `cname`, COUNT(*) AS `ocount`, SUM(`ocost`) AS `ocost` FROM `orders` LEFT JOIN `customer` ON `orders`.`cid` = `customer`.`cid` GROUP BY `orders`.`cid`");
+$clients = $result->fetch_all(MYSQLI_ASSOC);
+
+render_page("/admin/reports.tpl.php", "Reports", compact("products", "clients"),
+    extra_head: ['<script src="/assets/sortable.min.js" async defer></script>']);
--- htdocs/admin/reports.tpl.php
+++ htdocs/admin/reports.tpl.php
@@ -10,32 +10,96 @@
 
 <main class="m-4 mt-5">
     <div class="container is-max-desktop">
+
+        <h2 class="title is-3">Sales By Product</h2>
         <div class="table-container">
-            <table class="table is-striped is-hoverable is-fullwidth" style="text-wrap-mode: nowrap;">
+            <table class="table is-striped is-hoverable is-fullwidth" style="text-wrap-mode: nowrap;" data-sortable>
                 <thead>
                     <tr>
-                        <th>Product</th>
-                        <th>Total Orders</th>
-                        <th>Total Sales</th>
+                        <th data-sortable="false">#</th>
+                        <th>
+                            <div>
+                                <span>Product</span>
+                                <span class="icon is-small">
+                                    <i class="fa-solid fa-sort"></i>
+                                </span>
+                            </div>
+                        </th>
+                        <th>
+                            <div>
+                                <span>Total Orders</span>
+                                <span class="icon is-small">
+                                    <i class="fa-solid fa-sort"></i>
+                                </span>
+                            </div>
+                        </th>
+                        <th>
+                            <div>
+                                <span>Total Sales</span>
+                                <span class="icon is-small">
+                                    <i class="fa-solid fa-sort"></i>
+                                </span>
+                            </div>
+                        </th>
                     </tr>
                 </thead>
-                <tfoot>
-                    <tr>
-                        <th>Product</th>
-                        <th>Total Orders</th>
-                        <th>Total Sales</th>
-                    </tr>
-                </tfoot>
                 <tbody>
                     <? foreach ($products as $product): ?>
                         <tr>
-                            <th><?= $product["name"] ?></th>
-                            <td><?= $product["total_orders"] ?></td>
-                            <td><?= $product["total_sales"] ?></td>
+                            <th><?= $product["pid"] ?></th>
+                            <td data-value="<?= $product["pid"] ?>"><?= $product["pname"] ?></td>
+                            <td><?= $product["ocount"] ?></td>
+                            <td data-value="<?= $product["ocost"] ?>">$<?= sprintf("%.2f", $product["ocost"]) ?></td>
                         </tr>
                     <? endforeach ?>
                 </tbody>
             </table>
         </div>
+
+        <h2 class="title is-3">Sales By Client</h2>
+        <div class="table-container">
+            <table class="table is-striped is-hoverable is-fullwidth" style="text-wrap-mode: nowrap;" data-sortable>
+                <thead>
+                    <tr>
+                        <th data-sortable="false">#</th>
+                        <th>
+                            <div>
+                                <span>Client Name</span>
+                                <span class="icon is-small">
+                                    <i class="fa-solid fa-sort"></i>
+                                </span>
+                            </div>
+                        </th>
+                        <th>
+                            <div>
+                                <span>Total Orders</span>
+                                <span class="icon is-small">
+                                    <i class="fa-solid fa-sort"></i>
+                                </span>
+                            </div>
+                        </th>
+                        <th>
+                            <div>
+                                <span>Total Sales</span>
+                                <span class="icon is-small">
+                                    <i class="fa-solid fa-sort"></i>
+                                </span>
+                            </div>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <? foreach ($clients as $client): ?>
+                        <tr>
+                            <th><?= $client["cid"] ?></th>
+                            <td data-value="<?= $client["cid"] ?>"><?= $client["cname"] ?></td>
+                            <td><?= $client["ocount"] ?></td>
+                            <td data-value="<?= $client["ocost"] ?>">$<?= sprintf("%.2f", $client["ocost"]) ?></td>
+                        </tr>
+                    <? endforeach ?>
+                </tbody>
+            </table>
+        </div>
+
     </div>
 </main>
