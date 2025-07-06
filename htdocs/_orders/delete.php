0553fc635563bd0ac48d8b3a0b5ad2f0431103c4
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-07-07 01:41:18 +0800

    Added order delete for client

--- htdocs/_orders/delete.php
+++ htdocs/_orders/delete.php
@@ -1,10 +1,8 @@
 <?php
 
-require_once $_SERVER["DOCUMENT_ROOT"] . "/_global.php";
-
 $oid = $_POST["oid"] ?? $_GET["id"];
-$statement = $database->prepare("SELECT `oid`, `ostatus` FROM `orders` WHERE `cid` = ? AND `oid` = ? LIMIT 1");
-$statement->bind_param("ii", $_SESSION["user_id"], $oid);
+$statement = $database->prepare("SELECT `oid`, `ostatus`, `pname`, `odeliverdate` FROM `orders` LEFT JOIN `product` ON `orders`.`pid` = `product`.`pid` WHERE `oid` = ? LIMIT 1");
+$statement->bind_param("i", $oid);
 $statement->execute();
 
 $result = $statement->get_result();
@@ -15,5 +13,67 @@ if ($result->num_rows == 0)
     exit;
 }
 $order = $result->fetch_assoc();
+
+if (!empty($order["odeliverdate"]) && (strtotime($order["odeliverdate"]) < strtotime("+2 days")))
+{
+    http_response_code(400);
+    render_error_page("Invalid Request", "The order was no longer deletable due to processing constraints.");
+    exit;
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST")
+{
+    if (empty($_POST["oid"]))
+    {
+        http_response_code(400);
+        render_error_page("Invalid Request", "The request received was invalid.");
+        exit;
+    }
+
+    $statement = $database->prepare("SELECT `prodmat`.`mid`, `oqty` * `pmqty` AS `omqty` FROM `orders` CROSS JOIN `prodmat` ON `orders`.`pid` = `prodmat`.`pid` LEFT JOIN `material` ON `prodmat`.`mid` = `material`.`mid` WHERE `oid` = ?");
+    $statement->bind_param("i", $oid);
+    $statement->execute();
+
+    $result = $statement->get_result();
+    $materials = $result->fetch_all(MYSQLI_ASSOC);
+
+    $database->begin_transaction();
+    try
+    {
+        if ($order["ostatus"] == ORDER_STATUS_ACCEPTED)
+        {
+            foreach ($materials as $material)
+            {
+                $statement = $database->prepare("UPDATE `material` SET `mrqty` = `mrqty` - ? WHERE `mid` = ?");
+                $statement->bind_param("ii", $material["omqty"], $material["mid"]);
+                $statement->execute();
+            }
+        }
+
+        $statement = $database->prepare("DELETE FROM `orders` WHERE `oid` = ?");
+        $statement->bind_param("i", $_POST["oid"]);
+        $statement->execute();
+    
+        $statement->store_result();
+        if ($statement->affected_rows == 0)
+        {
+            $database->rollback();
+            http_response_code(500);
+            render_error_page("Order Not Deleted", "The request failed due to unknown reasons.");
+            exit;
+        }
+    }
+    catch (mysqli_sql_exception $ex)
+    {
+        $database->rollback();
+        http_response_code(500);
+        render_error_page(sprintf("MySQL Error %d", $ex->getCode()), $ex->getMessage());
+        exit;
+    }
+
+    $database->commit();
+    header("Location: /orders.php", true, 303);
+    exit;
+}
 
 render_page("/_orders/delete.tpl.php", "Delete Order", compact("order"), "warning");
--- htdocs/_orders/delete.tpl.php
+++ htdocs/_orders/delete.tpl.php
@@ -2,13 +2,13 @@
     <div class="hero-body">
         <form class="container is-max-desktop has-text-centered" action="<?= $_SERVER['REQUEST_URI'] ?>" method="post" type="application/x-www-form-urlencoded">
 
-            <input name="id" type="hidden" value="<?= $order["oid"] ?>" />
+            <input name="oid" type="hidden" value="<?= $order["oid"] ?>" />
 
             <h1 class="title">
                 Delete Order
             </h1>
             <p class="subtitle">
-                Are you sure to delete the order №<?= $order["oid"] ?>?
+                Are you sure to delete order №<?= $order["oid"] ?> of <?= $order["pname"] ?>?
                 <div class="buttons is-centered">
                     <button class="button is-danger is-dark" type="submit">
                         <span class="icon">
